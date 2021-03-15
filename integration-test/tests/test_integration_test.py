91a0d85edde3c7c0941d6e7d6aa06c1ca346b980
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2021-03-15 18:44:17 +0800

    asserting the validator address

--- integration-test/tests/test_integration_test.py
+++ integration-test/tests/test_integration_test.py
@@ -4,6 +4,7 @@ import subprocess
 import urllib.request
 import json
 import time
+from pathlib import Path
 
 def test_basic():
     tm = os.getenv('TENDERMINT')
@@ -15,9 +16,12 @@ def test_basic():
     contents = None
     start_time = time.perf_counter()
     timeout = 30
+    rpc_base = "http://127.0.0.1:26657"
+    status_url = rpc_base + "/status"
+    block_url = rpc_base + "/block"
     while True:
         try:
-            contents = urllib.request.urlopen("http://127.0.0.1:26657/status").read()
+            contents = urllib.request.urlopen(status_url).read()
             break
         except Exception as e:
             time.sleep(1)
@@ -27,8 +31,13 @@ def test_basic():
                 tmkms_err = tmkms_proc.stderr.readlines()
                 raise TimeoutError('Waited too long for the RPC port. tm: {}, tmkms: {} {}'.format(tm_output, tmkms_output, tmkms_err)) from e
     time.sleep(5)
-    contents = urllib.request.urlopen("http://127.0.0.1:26657/status").read()
+    contents = urllib.request.urlopen(status_url).read()
     status = json.loads(contents)
     block_height = int(status["result"]["sync_info"]["latest_block_height"])
-    # TODO: verify validator address / key matches tmkms one
     assert block_height >= 1
+    contents = urllib.request.urlopen(block_url).read()
+    block = json.loads(contents)
+    validator_address = block['result']['block']['last_commit']['signatures'][0]['validator_address']
+    genesis_path = tmhome + "/config/genesis.json"
+    genesis = json.loads(Path(genesis_path).read_text())
+    assert validator_address == genesis["validators"][0]["address"] 
