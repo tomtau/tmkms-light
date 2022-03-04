b35ecbf61c375c8f4356c597d0c8846f3f6dd152
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2022-03-04 03:28:40 +0000

    state errors

--- src/chain/state/error.rs
+++ src/chain/state/error.rs
@@ -3,23 +3,42 @@
 //! Modifications Copyright (c) 2021, Foris Limited (licensed under the Apache License, Version 2.0)
 
 use flex_error::define_error;
+use tendermint::block::{Height, Round};
 
 define_error! {
     StateError {
-        HeightRegressionError {}
-        |_| {
-            "height regression"
+        HeightRegressionError {
+            last_height: Height,
+            new_height: Height,
+        }
+        |e| {
+            format_args!("last height:{} new height:{}", e.last_height, e.new_height)
         },
 
-        StepRegressionError {} |_| {
-            "step regression"
+        RoundRegressionError {
+            height: Height,
+            last_round: Round,
+            new_round: Round,
+        } |e| {
+            format_args!("round regression at height:{} last round:{} new round:{}", e.height, e.last_round, e.new_round)
         },
 
-        RoundRegressionError {} |_| {
-            "round regression"
+        StepRegressionError {
+            height: Height,
+            round: Round,
+            last_step: i8,
+            new_step: i8,
+        } |e| {
+            format_args!("round regression at height:{} round:{} last step:{} new step:{}", e.height, e.round, e.last_step, e.new_step)
         },
-        DoubleSignError{} |_| {
-            "double sign detected"
+        DoubleSignError{
+            height: Height,
+            round: Round,
+            step: i8,
+            old_block_id: String,
+            new_block_id: String,
+        } |e| {
+            format_args!("Attempting to sign a second proposal at height:{} round:{} step:{} old block id:{} new block {}", e.height, e.round, e.step, e.old_block_id, e.new_block_id)
         },
         SyncError{} |_| {
             "error syncing state"
--- src/rpc.rs
+++ src/rpc.rs
@@ -38,29 +38,42 @@ impl Request {
 
         // Parse Protobuf-encoded request message
         let msg = PrivMessage::decode_length_delimited(msg.as_ref())
-            .map_err(|e| Error::protocol_error("malformed message packet: {}".into(),e.into()))?
+            .map_err(|e| Error::protocol_error("malformed message packet: {}".into(), e.into()))?
             .sum;
 
         match msg {
             Some(Sum::SignVoteRequest(req)) => {
                 let svr = SignVoteRequest::try_from(req).map_err(|e| {
-                    Error::protocol_error_tendermint("sign vote request domain type error: {}".into(), e)})?;
+                    Error::protocol_error_tendermint(
+                        "sign vote request domain type error: {}".into(),
+                        e,
+                    )
+                })?;
                 Ok(Request::SignVote(svr))
             }
             Some(Sum::SignProposalRequest(spr)) => {
                 let spr = SignProposalRequest::try_from(spr).map_err(|e| {
-                    Error::protocol_error_tendermint("sign proposal request domain type error: {}".into(), e)
+                    Error::protocol_error_tendermint(
+                        "sign proposal request domain type error: {}".into(),
+                        e,
+                    )
                 })?;
                 Ok(Request::SignProposal(spr))
             }
             Some(Sum::PubKeyRequest(pkr)) => {
                 let pkr = PubKeyRequest::try_from(pkr).map_err(|e| {
-                    Error::protocol_error_tendermint("pubkey request domain type error: {}".into(), e)
+                    Error::protocol_error_tendermint(
+                        "pubkey request domain type error: {}".into(),
+                        e,
+                    )
                 })?;
                 Ok(Request::ShowPublicKey(pkr))
             }
             Some(Sum::PingRequest(pr)) => Ok(Request::ReplyPing(pr)),
-            _ => Err(Error::protocol_error_msg("invalid RPC message: {:?}".into(), msg)),
+            _ => Err(Error::protocol_error_msg(
+                "invalid RPC message: {:?}".into(),
+                msg,
+            )),
         }
     }
 }
@@ -175,10 +188,7 @@ impl Response {
 
         PrivMessage { sum: Some(msg) }
             .encode_length_delimited(&mut buf)
-            .map_err(|e| {
-                Error::protocol_error("failed to encode response: {}".into(), e.into())
-
-            })?;
+            .map_err(|e| Error::protocol_error("failed to encode response: {}".into(), e.into()))?;
         Ok(buf)
     }
 }
